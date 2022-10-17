a814ca562f3ec58215f1aa5093037b5d1e5dcca0
liang799 <71926348+liang799@users.noreply.github.com>
2022-10-17 22:53:38 +0800

    Refactor test pt1

--- Money-Example/money.test.ts
+++ Money-Example/money.test.ts
@@ -4,9 +4,9 @@ import { test, afterAll, beforeAll, describe, expect, it } from "@jest/globals";
 test("test multiplication", () => {
     const five = new Dollar(5);
     let product = five.times(2);
-    expect(product.amount).toBe(10);
+    expect(product).toBe(new Dollar(10));
     product = five.times(3);
-    expect(product.amount).toBe(product.amount);
+    expect(product.amount).toBe(new Dollar(15));
 })
 
 /* Triangulation: use it only when uncertain on how to refactor */
