a17ca8d3957e07ba8826f07da3efc8cc518f8c90
liang799 <71926348+liang799@users.noreply.github.com>
2022-10-17 22:10:39 +0800

    Passed test by returning an object

--- Money-Example/multiply.test.ts
+++ Money-Example/multiply.test.ts
@@ -3,6 +3,8 @@ import { test, afterAll, beforeAll, describe, expect, it } from "@jest/globals";
 
 test("test multiplication", () => {
     const five = new Dollar(5);
-    five.times(2);
-    expect(five.amount).toBe(10);
+    let product = five.times(2);
+    expect(product.amount).toBe(10);
+    product = five.times(3);
+    expect(product.amount).toBe(product.amount);
 })
